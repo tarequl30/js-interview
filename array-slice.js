55139949ce4e57ab731fe5aa1da5f02b62b810d8
Tareq <tarequl30@gmail.com>
2021-02-03 00:04:06 +0600

    another doc

---
+++ array-slice.js
@@ -0,0 +1,8 @@
+const nums = [9,5,6,7,3,1,4,5,7];
+const part = nums.slice(2,8);
+console.log(part);
+console.log(nums);
+
+const removed = nums.splice(2,4);
+console.log(removed);
+console.log(nums);
